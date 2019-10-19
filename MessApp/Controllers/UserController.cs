aebde5c96228cb98b39d179921ef2c9ddd4101e1
Jasper van der Zwaan <jasper.vanderzwaan.jvdz@outlook.com>
2019-10-19 17:34:57 +0200

    - Added usercontroller - Deleted user birthdate property

---
+++ MessApp/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Models.Datamodels;
+
+namespace MessApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserRepository UserRepository;
+        private readonly MessageRepository MessageRepository;
+
+        public UserController(IUserContext ucontext, IMessageContext mcontext)
+        {
+            UserRepository = new UserRepository(ucontext);
+            MessageRepository = new MessageRepository(mcontext);
+        }
+
+        public IActionResult Account(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                return View();
+            }
+            return View();
+        }
+
+        public IActionResult Authenticate(User user = null)
+        {
+            if (!string.IsNullOrEmpty(user.Username) && !string.IsNullOrEmpty(user.Password))
+            {
+                if (UserRepository.Authenticate(user))
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Register(User user = null)
+        {
+            if (!string.IsNullOrEmpty(user.Username) && !string.IsNullOrEmpty(user.Password) && !string.IsNullOrEmpty(user.Email))
+            {
+                if (UserRepository.Insert(user))
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
--- Models/DataModels/User.cs
+++ Models/DataModels/User.cs
@@ -7,6 +7,5 @@ namespace Models.Datamodels
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public DateTime Birthdate { get; set; }
     }
 }
